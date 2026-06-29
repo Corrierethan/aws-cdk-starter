3bc32139cabbe2767b41209e2e4e07b346cda8dc
Ethan <corrierethan@icloud.com>
2026-06-28 22:19:48 -0500

    test: uncomment and expand unit tests for SQS, S3, and Lambda resources

--- cdk_src/tests/unit/test_cdk_src_stack.py
+++ cdk_src/tests/unit/test_cdk_src_stack.py
@@ -3,13 +3,46 @@ import aws_cdk.assertions as assertions
 
 from cdk_src.cdk_src_stack import CdkSrcStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in cdk_src/cdk_src_stack.py
-def test_sqs_queue_created():
+
+def _template():
     app = core.App()
     stack = CdkSrcStack(app, "cdk-src")
-    template = assertions.Template.from_stack(stack)
+    return assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+
+def test_sqs_queue_created():
+    template = _template()
+    template.has_resource_properties("AWS::SQS::Queue", {
+        "VisibilityTimeout": 300
+    })
+
+
+def test_s3_bucket_versioning_enabled():
+    template = _template()
+    template.has_resource_properties("AWS::S3::Bucket", {
+        "VersioningConfiguration": {"Status": "Enabled"}
+    })
+
+
+def test_s3_bucket_blocks_public_access():
+    template = _template()
+    template.has_resource_properties("AWS::S3::Bucket", {
+        "PublicAccessBlockConfiguration": {
+            "BlockPublicAcls": True,
+            "BlockPublicPolicy": True,
+            "IgnorePublicAcls": True,
+            "RestrictPublicBuckets": True,
+        }
+    })
+
+
+def test_lambda_runtime_is_python312():
+    template = _template()
+    template.has_resource_properties("AWS::Lambda::Function", {
+        "Runtime": "python3.12"
+    })
+
+
+def test_lambda_sqs_event_source_mapping():
+    template = _template()
+    template.resource_count_is("AWS::Lambda::EventSourceMapping", 1)
